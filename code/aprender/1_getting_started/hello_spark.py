4e9944f795068146fec70429ba09385722cbf1b0
oksm <osanchezk@gmail.com>
2023-02-21 03:35:23 +0000

    Ejercicios con spark

--- code/aprender/1_getting_started/hello_spark.py
+++ code/aprender/1_getting_started/hello_spark.py
@@ -5,9 +5,9 @@ from pyspark.sql import SparkSession
 if __name__ == "__main__":
     spark = SparkSession\
         .builder\
-        .appName("hello_spark")\
+        .appName("hello_spark_omar")\
         .getOrCreate()
 
-    print("Hello spark world, welcome <your-name> ... ")
+    print("Hello spark world, welcome Omar K ... ")
 
     spark.stop()
